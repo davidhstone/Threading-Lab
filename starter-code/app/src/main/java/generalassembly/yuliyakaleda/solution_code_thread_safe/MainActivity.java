23292f56fd40c9bcf26aa4ba51c734fcc0a065fb
davidhstone <d_dave27@hotmail.com>
2016-08-03 01:11:59 -0400

    Dave Stone Threading Lab... it works

--- starter-code/app/src/main/java/generalassembly/yuliyakaleda/solution_code_thread_safe/MainActivity.java
+++ starter-code/app/src/main/java/generalassembly/yuliyakaleda/solution_code_thread_safe/MainActivity.java
@@ -1,6 +1,7 @@
 package generalassembly.yuliyakaleda.solution_code_thread_safe;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity{
   private ImageView mImageView;
   private Button mChooseButton;
   private ProgressBar mProgressBar;
+  private ImageProcessingAsyncTask mTask;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +52,12 @@ public class MainActivity extends AppCompatActivity{
     if (requestCode == PICK_IMAGE_REQUEST && resultCode == MainActivity.RESULT_OK && null != data) {
       Uri selectedImage = data.getData();
       //TODO: Create the async task and execute it
+
+      ImageProcessingAsyncTask task = new ImageProcessingAsyncTask();
+      task.execute(selectedImage);
+
     }
+
   }
 
   // brings up the photo gallery/other resources to choose a picture
@@ -62,11 +69,11 @@ public class MainActivity extends AppCompatActivity{
   }
 
   //TODO: Fill in the parameter types
-  private class ImageProcessingAsyncTask extends AsyncTask<> {
+  private class ImageProcessingAsyncTask extends AsyncTask<android.net.Uri, Integer, Bitmap> {
 
     //TODO: Fill in the parameter type
     @Override
-    protected Bitmap doInBackground() {
+    protected Bitmap doInBackground(Uri... params) {
       try {
         Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(params[0]));
         return invertImageColors(bitmap);
@@ -78,21 +85,26 @@ public class MainActivity extends AppCompatActivity{
 
     //TODO: Fill in the parameter type
     @Override
-    protected void onProgressUpdate() {
+    protected void onProgressUpdate(Integer... values) {
       super.onProgressUpdate(values);
       //TODO: Update the progress bar
+      mProgressBar.setProgress(values[0]);
     }
 
     //TODO: Fill in the parameter type
     @Override
-    protected void onPostExecute() {
+    protected void onPostExecute(Bitmap bitmap) {
       //TODO: Complete this method
+      mImageView.setImageBitmap(bitmap);
+
+      mProgressBar.setVisibility(View.INVISIBLE);
     }
 
     @Override
     protected void onPreExecute() {
       super.onPreExecute();
       //TODO: Complete this method
+      mProgressBar.setVisibility(View.VISIBLE);
     }
 
     private Bitmap invertImageColors(Bitmap bitmap){
@@ -104,9 +116,13 @@ public class MainActivity extends AppCompatActivity{
         for(int j = 0; j < mutableBitmap.getHeight(); j++){
           //TODO: Get the Red, Green, and Blue values for the current pixel, and reverse them
           //TODO: Set the current pixel's color to the new, reversed value
+
+          int color = mutableBitmap.getPixel(i, j);
+          mutableBitmap.setPixel(i, j, (0xFFFFF - color) | 0xFF000000);
         }
         int progressVal = Math.round((long) (100*(i/(1.0*mutableBitmap.getWidth()))));
         //TODO: Update the progress bar. progressVal is the current progress value out of 100
+        publishProgress(progressVal);
       }
       return mutableBitmap;
     }
